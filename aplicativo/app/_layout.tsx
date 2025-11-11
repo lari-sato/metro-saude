b1cf0cd65f2756a36266ec2d313d8a215ab7744b
j.mack <10417672@mackenzista.com.br>
2025-11-11 01:07:39 -0300

    Configuração de Páginas e Rodapé de Navegação

--- aplicativo/app/_layout.tsx
+++ aplicativo/app/_layout.tsx
@@ -1,5 +1,57 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import MapIcon from "../assets/icons/map.svg";
+import UserIcon from "../assets/icons/user.svg";
+import SettingsIcon from "../assets/icons/settings.svg";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#1C5CA2",
+        tabBarInactiveTintColor: "#000000",
+         tabBarStyle: {
+          backgroundColor: "#D9D9D9", 
+          height: 60,  
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{ href: null 
+        }} 
+      />
+
+      <Tabs.Screen
+        name="pages/navegar"
+        options={{
+          title: "Navegar",
+          tabBarIcon: ({ color }) => (
+            <MapIcon width={28} height={28} stroke={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="pages/perfil"
+        options={{
+          title: "Perfil",
+          tabBarIcon: ({ color }) => (
+            <UserIcon width={28} height={28} stroke={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="pages/configuracoes"
+        options={{
+          title: "Configurações",
+          tabBarIcon: ({ color }) => (
+            <SettingsIcon width={28} height={28} stroke={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
---
+++ aplicativo/app/pages/navegar.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Navegar() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Tela Navegar</Text>
+    </View>
+  );
+}
